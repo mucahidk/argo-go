5fe4613ebb2d564b43a682d074662a3bcd61a906
mucahidk <59875802+mucahidk@users.noreply.github.com>
2022-02-02 10:16:14 +0300

    Update main.go
    check in

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	fmt.Println("Go Docker Tutorial")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello Docker from Jenkins on ArgoCD...Naber moruque")
+		fmt.Fprintf(w, "Hello Docker from Jenkins on ArgoCD...Naber  moruque")
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
